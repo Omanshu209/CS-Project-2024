ebd95d1bfe7dd57556476e911902a33dde43fd8b
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-10-10 10:00:11 +0530

    Create `Q21.java`

---
+++ Q21.java
@@ -0,0 +1,11 @@
+public class Q21
+{
+	public static void main(String[] args)
+	{
+		for(int i = 1 ; i <= 26 ; i++)
+		{
+			int ASCII = 64 + i;
+			System.out.println(((char) ASCII) + " : " + i); 
+		}
+	}
+}
